2e31f24073cc03f878fc6fa749a9d84b0a3e3df5
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-02 19:26:55 +0900

    useMemoとuseCallback

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { memo, useState } from 'react'
+import { memo, useCallback, useMemo, useState } from 'react'
 
 // eslint-disable-next-line react/display-name
 const Child1 = memo(() => {
@@ -11,19 +11,42 @@ const Child1 = memo(() => {
   )
 })
 
-const Child2 = () => {
+type Child2Props = {
+  handleClick: () => void
+}
+// eslint-disable-next-line react/display-name
+const Child2 = memo(({ handleClick }: Child2Props) => {
   return (
     <>
       <p>Child2コンポーネントです</p>
+      <button
+        className="mt-5 py-2 px-4 border-2 border-slate-200 rounded-md"
+        onClick={handleClick}
+      >
+        Click
+      </button>
     </>
   )
-}
+})
 
 const Home = () => {
   const [text, setText] = useState('')
+  const [count, setCount] = useState(0)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value)
+
+  // Propsとして渡されるがuseCallbackがないとtextなどState更新時にこの関数も再生成され、子コンポーネントも再レンダリングされる（関数をキャッシュするため）
+  const handleClick = useCallback(() => console.log('click'), [])
+
+  const double = (count: number) => {
+    let i = 0
+    while (i < 3000000000) i++
+    return count * 2
+  }
+
+  // countの変更のレンダリングには時間が掛かるが、textの更新によるレンダリングには時間がかからない（doubleCountがキャッシュされる）
+  const doubleCount = useMemo(() => double(count), [count])
 
   return (
     <div>
@@ -35,7 +58,17 @@ const Home = () => {
         onChange={handleChange}
       />
       <Child1 />
-      <Child2 />
+      <Child2 handleClick={handleClick} />
+      <p>親コンポーネントで重い計算処理</p>
+      <p>
+        Counter: {count}, {doubleCount}{' '}
+      </p>
+      <button
+        className="border-2 border-slate-200 rounded-md py-2 px-4 mt-5"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment Count2
+      </button>
     </div>
   )
 }
